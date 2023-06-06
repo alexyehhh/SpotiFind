f693528db31271087eaa526fd9bc219b26746b5b
“Alex <alpyeh@ucsc.edu>
2023-06-05 18:35:52 -0700

    Update files

--- api.js
+++ api.js
@@ -1,69 +1,60 @@
-// Import the Express.js library. Express is a web application framework for Node.js.
-const express = require('express');
+const express = require('express');// import the Express.js library
+const request = require('request');// import the request library
 
-// Import the 'request' library. 'request' is a simple way to make HTTP calls. It supports HTTPS and follows redirects by default.
-const request = require('request');
+const app = express();// create an instance of an Express application to make endpoints for server
 
-// Create an instance of an Express application. This will be used to set up the endpoints for our server.
-const app = express();
-
-// Define constants for the client ID, client secret, and redirect URI of your Spotify app.
-const client_id = 'bdda1770a6dd4e01ba0bb6aaa5886b11'; 
+// define constants for the client ID, client secret, and redirect URI
+const client_id = 'bdda1770a6dd4e01ba0bb6aaa5886b11';
 const client_secret = 'a8d60c3b750c4c898b735f122f8f9112';
 const redirect_uri = 'http://localhost:3000/callback'; 
 
-// Define a route for the '/login' endpoint. This will redirect the user to the Spotify authorization page.
-app.get('/login', (req, res) => {
-  const scopes = 'playlist-modify-public'; // Define the scopes (permissions) the app will request
-  res.redirect('https://accounts.spotify.com/authorize' + // Redirect the user to the Spotify authorization page
-    '?response_type=code' + // Specify that your app needs an authorization code
-    '&client_id=' + client_id + // Append your app's client ID
-    '&scope=' + encodeURIComponent(scopes) + // Append the scopes
-    '&redirect_uri=' + encodeURIComponent(redirect_uri)); // Append the redirect URI
+app.get('/login', (req, res) => {// define a route for the '/login' endpoint to redirect to the spotfiy authorization page
+  const scopes = 'playlist-modify-public'; // define the permissions the app will request
+  res.redirect('https://accounts.spotify.com/authorize' + // redirect the user to the Spotify authorization page
+    '?response_type=code' + // specify that your app needs an authorization code
+    '&client_id=' + client_id + // ppend your app client ID
+    '&scope=' + encodeURIComponent(scopes) + // append the scopes
+    '&redirect_uri=' + encodeURIComponent(redirect_uri)); // append the redirect URI
 });
 
-// Define a route for the '/callback' endpoint. This will handle the redirection from Spotify after the user authorizes the app.
-app.get('/callback', (req, res) => {
-  const code = req.query.code || null; // Get the authorization code from the request parameters
+app.get('/callback', (req, res) => {//define a route for the callback endpoint. This will handle the redirection from Spotify after the user authorizes the app
+  const code = req.query.code || null; // get the authorization code from the request parameters
 
-  // Define the options for the request to exchange the authorization code for an access token.
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token', // The token endpoint
+  const authOptions = { // define the options for the request to exchange the authorization code for an access token
+    url: 'https://accounts.spotify.com/api/token', // the token endpoint
     form: {
-      code: code, // The authorization code
-      redirect_uri: redirect_uri, // The redirect URI
-      grant_type: 'authorization_code' // The grant type
+      code: code, // authorization code
+      redirect_uri: redirect_uri, // redirect URI
+      grant_type: 'authorization_code' // grant type
     },
     headers: {
-      'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')) // The client credentials
+      'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')) // client credentials
     },
-    json: true // Specify that the response should be JSON
+    json: true // specify that the response should be JSON
   };
 
-  // Make the request to get the access token
-  request.post(authOptions, (error, response, body) => {
-    if (!error && response.statusCode === 200) { // If the request was successful
-      const access_token = body.access_token; // Get the access token from the response
-      const refresh_token = body.refresh_token; // Get the refresh token from the response
-      // The tokens can now be stored for later use
-      res.redirect('/success'); // Redirect to a success page
-    } else { // If the request failed
-      res.redirect('/error'); // Redirect to an error page
+  request.post(authOptions, (error, response, body) => { // make the request to get the access token
+    if (!error && response.statusCode === 200) { // if the request was successful
+      const access_token = body.access_token; // get the access token from the response
+      const refresh_token = body.refresh_token; // get the refresh token from the response
+      // Store the the tokens
+      res.redirect('/success'); // redirect to a success page
+    } else { // if the request failed
+      res.redirect('/error'); // redirect to an error page
     }
   });
 });
 
-// Define a route for the '/success' endpoint. This can be used to render a success page after a successful authentication.
+// define a route for the success endpoint. This can be used to render a success page after a successful authentication.
 app.get('/success', (req, res) => {
-  res.send('Authentication successful! New buttons/UI elements can be displayed here.');
+  res.redirect('https://alexyehhh.github.io/spotifind/?authenticated=true'); // Redirect to the main website with query parameter
 });
 
-// Define a route for the '/error' endpoint. This can be used to render an error page after a failed authentication.
-app.get('/error', (req, res) => {
+
+app.get('/error', (req, res) => {// define a route for the error endpoint. Display error page
   res.send('Authentication failed. Please try again.');
 });
 
-// Start the server, making it listen for requests on port 3000.
-app.listen(3000, () => {
+app.listen(3000, () => {// start the server making it listen for requests on port 3000.
 	console.log('Server is running on port 3000');
   });
--- script.js
+++ script.js
@@ -1,144 +1,61 @@
-//Create the variable for latitude
-
-
-// window.onload = function(){
-//     const date = new Date();
-//     const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-//     document.getElementById('date').innerHTML = dateString;
-
-// if ("geolocation" in navigator) {
-//     navigator.geolocation.getCurrentPosition(success)
-
-// } else {
-//   console.log("Geolocation is not available in your browser.");
-// }
-// }
-
-
 
 const btn = document.getElementById('connectBtn');
 btn.addEventListener("click", function() {
-  // Redirect the user to the Spotify authorization page
-  window.location.href = 'http://localhost:3000/login';
+  window.location.href = 'http://localhost:3000/login';// redirect the user to the Spotify authorization page
 });
 
 
-// Function to hide an element
+// function to hide an element
 function hideElement(elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-      element.classList.add('hidden');
-    }
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.style.display = 'none';
   }
-  
-  // Function to show an element
-  function showElement(elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-      element.classList.remove('hidden');
-    }
+}
+
+// function to show an element
+function showElement(elementId) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.style.display = 'block';
   }
+}
+
   
-  // Check if access token and refresh token are available in localStorage
+  // check if access token and refresh token are available in localStorage
   const accessToken = localStorage.getItem('accessToken');
   const refreshToken = localStorage.getItem('refreshToken');
   
   // Function to handle successful authentication
+
+  // if success run this
   function handleAuthentication() {
-    hideElement('connectBtn'); // Hide the "Connect" button
-    showElement('newButton1'); // Show the new button 1
-    showElement('newButton2'); // Show the new button 2
-    showElement('newButton3'); // Show the new button 3
-    showElement('newButton4'); // Show the new button 4
+    hideElement('connectBtn'); // hide the "Connect" button
+    showElement('newButton1'); // show the new button 1
+    showElement('newButton2'); // show the new button 2
+    showElement('newButton3'); // show the new button 3
+    showElement('newButton4'); // show the new button 4
   }
   
   // Attach event listener to the "Connect" button
   const connectBtn = document.getElementById('connectBtn');
   connectBtn.addEventListener('click', () => {
-    // Simulating successful authentication
-    localStorage.setItem('accessToken', 'YOUR_ACCESS_TOKEN');
+    localStorage.setItem('accessToken', 'YOUR_ACCESS_TOKEN');// simulating successful authentication
     localStorage.setItem('refreshToken', 'YOUR_REFRESH_TOKEN');
   
-    handleAuthentication();
+   // handleAuthentication();
   });
 
-// const btn = document.getElementById('getmood1');
-// btn.addEventListener("click",function(){    
-//     const xhr = new XMLHttpRequest();
 
-//     xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`);
-//     xhr.send();
+  window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const isAuthenticated = urlParams.get('authenticated');
 
-//     xhr.onload = function() {
-// 	    const body = JSON.parse(xhr.responseText);
-//         let temperature = body.temperature;
-// 	    let weatherStatus = body.weatherStatus;
-//         document.getElementById("temperature").innerHTML = `Temperature: ${temperature}\u00B0F`;
-//         document.getElementById('weatherStatus').innerHTML = `weather Status: ${weatherStatus}`;
-// }
-// }
-
-// const btn = document.getElementById('getmood2');
-// btn.addEventListener("click",function(){    
-//     const xhr = new XMLHttpRequest();
-
-//     xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`);
-//     xhr.send();
-
-//     xhr.onload = function() {
-// 	    const body = JSON.parse(xhr.responseText);
-//         let temperature = body.temperature;
-// 	    let weatherStatus = body.weatherStatus;
-//         document.getElementById("temperature").innerHTML = `Temperature: ${temperature}\u00B0F`;
-//         document.getElementById('weatherStatus').innerHTML = `weather Status: ${weatherStatus}`;
-// }
-// }
-
-// const btn = document.getElementById('getmood3');
-// btn.addEventListener("click",function(){    
-//     const xhr = new XMLHttpRequest();
-
-//     xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`);
-//     xhr.send();
-
-//     xhr.onload = function() {
-// 	    const body = JSON.parse(xhr.responseText);
-//         let temperature = body.temperature;
-// 	    let weatherStatus = body.weatherStatus;
-//         document.getElementById("temperature").innerHTML = `Temperature: ${temperature}\u00B0F`;
-//         document.getElementById('weatherStatus').innerHTML = `weather Status: ${weatherStatus}`;
-// }
-// }
-
-// const btn = document.getElementById('getmood4');
-// btn.addEventListener("click",function(){    
-//     const xhr = new XMLHttpRequest();
-
-//     xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`);
-//     xhr.send();
-
-//     xhr.onload = function() {
-// 	    const body = JSON.parse(xhr.responseText);
-//         let temperature = body.temperature;
-// 	    let weatherStatus = body.weatherStatus;
-//         document.getElementById("temperature").innerHTML = `Temperature: ${temperature}\u00B0F`;
-//         document.getElementById('weatherStatus').innerHTML = `weather Status: ${weatherStatus}`;
-// }
-// }
-
-
-//-------------------------------?-----------------------------------------
-// const xhr2 = new XMLHttpRequest();
-// xhr2.open("GET", `http://localhost:3000/5day/${latitude}/${longitude}`);
-// xhr2.send();
-
-// xhr2.onload = function() {
-// 	const body = JSON.parse(xhr2.responseText);
-// 	let forecast = body;
-//     let forecastElements = document.getElementsByClassName("forecast");
-// 	for (let i = 0; i < forecast.length; i++) {
-// 		forecastElements[i].innerHTML = forecast[i].dayName + ": " + forecast[i].temp + "\u00B0F";
-//         }
-//     } 
-
-// })
+  
+    if (isAuthenticated === 'true') { //access token check 
+      handleAuthentication();
+    }
+    else {
+      res.redirect('https://alexyehhh.github.io/spotifind/');
+    }
+  };
