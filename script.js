4101c6644594e650a9f7b1749fe1b922b102def3
Diego Yanez <130004197+OceanBIvd@users.noreply.github.com>
2023-06-13 14:24:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,41 @@ btn.addEventListener("click", function() {
   window.location.href = 'http://localhost:3000/login';
 });
 
+function hideAll() {
+  hideElement('applicationDescription');
+  hideElement('connectDescription');
+  hideElement('connectBtn');
+  hideElement('newButton1');
+  hideElement('newButton2');
+  hideElement('newButton3');
+  hideElement('newButton4');
+  hideElement('emote1');
+  hideElement('emote2');
+  hideElement('emote3');
+  hideElement('emote4');
+  hideElement('playlistContainer');
+  const element = document.querySelector('.emojis');
+  if (element) {
+    element.style.display = 'none';
+  }
+}
 
-// function to hide an element
+function hideHome() {
+  hideElement('newButton1');
+  hideElement('newButton2');
+  hideElement('newButton3');
+  hideElement('newButton4');
+  hideElement('emote1');
+  hideElement('emote2');
+  hideElement('emote3');
+  hideElement('emote4');
+  hideElement('playlistContainer');
+  const element = document.querySelector('.emojis');
+  if (element) {
+    element.style.display = 'none';
+  }
+}
+
 function hideElement(elementId) {
   const element = document.getElementById(elementId);
   if (element) {
@@ -12,7 +45,13 @@ function hideElement(elementId) {
   }
 }
 
-// function to show an element
+function showEmojiContainer() {
+  const element = document.querySelector('.emojis');
+  if (element) {
+    element.style.display = 'flex';
+  }
+}
+
 function showElement(elementId) {
   const element = document.getElementById(elementId);
   if (element) {
@@ -20,139 +59,279 @@ function showElement(elementId) {
   }
 }
 
-  
-  // check if access token and refresh token are available in localStorage
+function handleAuthentication() {
+  hideElement('connectBtn');
+  showElement('newButton1');
+  showElement('newButton2');
+  showElement('newButton3');
+  showElement('newButton4');
+  showElement('emote1');
+  showElement('emote2');
+  showElement('emote3');
+  showElement('emote4');
+  hideElement('connectDescription');
+  showEmojiContainer();
+}
+
+const connectBtn = document.getElementById('connectBtn');
+connectBtn.addEventListener('click', () => {
+  localStorage.setItem('accessToken', getAccessToken());
+  localStorage.setItem('refreshToken', getRefreshToken());
+});
+
+window.onload = function() {
   const accessToken = localStorage.getItem('accessToken');
   const refreshToken = localStorage.getItem('refreshToken');
-  
-  // Function to handle successful authentication
 
-  // if success run this
-  function handleAuthentication() {
-    hideElement('connectBtn'); // hide the "Connect" button
-    showElement('newButton1'); // show the new button 1
-    showElement('newButton2'); // show the new button 2
-    showElement('newButton3'); // show the new button 3
-    showElement('newButton4'); // show the new button 4
+  const urlParams = new URLSearchParams(window.location.search);
+  const isAuthenticated = urlParams.get('authenticated');
+
+  if (accessToken && refreshToken || isAuthenticated === 'true') {
+    handleAuthentication();
+  } else {
+    hideHome();
   }
-  
-  // Attach event listener to the "Connect" button
-  const connectBtn = document.getElementById('connectBtn');
-  connectBtn.addEventListener('click', () => {
-    localStorage.setItem('accessToken', 'YOUR_ACCESS_TOKEN');// simulating successful authentication
-    localStorage.setItem('refreshToken', 'YOUR_REFRESH_TOKEN');
-  
-   // handleAuthentication();
-  });
+};
 
-
-  window.onload = function() {
-    // check if access token and refresh token are available in localStorage
-    const accessToken = localStorage.getItem('accessToken');
-    const refreshToken = localStorage.getItem('refreshToken');
-  
-    if (accessToken && refreshToken) {
-      handleAuthentication();
-    }
-    // else part removed
-  };
-
-/*
-// Retrieve access token from localStorage
-const accessToken = localStorage.getItem('accessToken');
-
-function getRecommendations(minDanceability, maxDanceability, minEnergy, maxEnergy, minInstrumentalness, maxInstrumentalness) {
-  // Prepare the API request URL with the specified parameters
-  const url = `https://api.spotify.com/v1/recommendations?seed_genres=pop&min_danceability=${minDanceability}&max_danceability=${maxDanceability}&min_energy=${minEnergy}&max_energy=${maxEnergy}&min_instrumentalness=${minInstrumentalness}&max_instrumentalness=${maxInstrumentalness}`;
-
-  // Send the API request
-  fetch(url, {
+document.getElementById("newButton1").addEventListener("click", function() {
+  fetch('http://localhost:3000/generate-happy-playlist', {
     headers: {
-      'Authorization': `Bearer ${accessToken}`
+      'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
     }
   })
-    .then(response => response.json())
-    .then(data => {
-      // Handle the recommended tracks data
-      const tracks = data.tracks;
-      const randomSongs = getRandomSongs(tracks, 5); // Example: Select 5 random songs
-
-      // Get the user's Spotify user ID
-      fetch('https://api.spotify.com/v1/me', {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`
-        }
-      })
-        .then(response => response.json())
-        .then(userData => {
-          const userId = userData.id;
-          console.log('User ID:', userId);
-
-          // Call the addSongsToPlaylist function with the random songs and user ID
-          addSongsToPlaylist(randomSongs, userId);
-        })
-        .catch(error => {
-          // Handle any errors
-          console.error(error);
-        });
-    })
-    .catch(error => {
-      // Handle any errors
-      console.error(error);
-    });
-}
-
-function addSongsToPlaylist(songs, userId) {
-  // Create the playlist
-  fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      'name': 'My Random Playlist',
-      'description': 'A playlist created by SpotiFind',
-      'public': true
-    })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
   })
-    .then(response => response.json())
-    .then(playlistData => {
-      const playlistId = playlistData.id;
+  .then(trackDetails => {
+    if (Array.isArray(trackDetails)) {
+      hideAll();
 
-      // Create an array of track URIs
-      const uris = songs.map(song => song.uri);
+      const playlistContainer = document.getElementById('playlistContainer');
+      if (playlistContainer) {
+        playlistContainer.style.display = 'block';
+      }
 
-      // Add songs to the playlist
-      fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+      const playlist = document.getElementById('playlist');
+      playlist.innerHTML = '';
+
+      trackDetails.forEach(track => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<img src="${track.albumCover}" class="album-cover"><span>${track.name} by ${track.artist}</span>`;
+        playlist.appendChild(listItem);
+      });
+
+      const playlistName = prompt('Enter a name for your playlist:');
+      return fetch('http://localhost:3000/export-playlist', {
         method: 'POST',
         headers: {
-          'Authorization': `Bearer ${accessToken}`,
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
         },
         body: JSON.stringify({
-          'uris': uris
+          playlistName: playlistName,
+          trackURIs: trackDetails.map(track => track.uri)
         })
-      })
-        .then(response => {
-          if (response.ok) {
-            console.log('Songs added to the playlist successfully!');
-          } else {
-            console.log('Failed to add songs to the playlist.');
-          }
+      });
+    } else {
+      console.error('Error: trackURIs is not an array:', trackDetails);
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Playlist successfully exported!');
+    } else {
+      throw new Error('Failed to export the playlist.');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert("There was an error generating your playlist. Please try again later.");
+  });
+});
+
+document.getElementById("newButton2").addEventListener("click", function() {
+  fetch('http://localhost:3000/generate-excited-playlist', {
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(trackDetails => {
+    if (Array.isArray(trackDetails)) {
+      hideAll();
+
+      const playlistContainer = document.getElementById('playlistContainer');
+      if (playlistContainer) {
+        playlistContainer.style.display = 'block';
+      }
+
+      const playlist = document.getElementById('playlist');
+      playlist.innerHTML = '';
+
+      trackDetails.forEach(track => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<img src="${track.albumCover}" class="album-cover"><span>${track.name} by ${track.artist}</span>`;
+        playlist.appendChild(listItem);
+      });
+
+      const playlistName = prompt('Enter a name for your playlist:');
+      return fetch('http://localhost:3000/export-playlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        },
+        body: JSON.stringify({
+          playlistName: playlistName,
+          trackURIs: trackDetails.map(track => track.uri)
         })
-        .catch(error => {
-          // Handle any errors
-          console.error(error);
-        });
-    })
-    .catch(error => {
-      // Handle any errors
-      console.error(error);
-    });
-}
+      });
+    } else {
+      console.error('Error: trackURIs is not an array:', trackDetails);
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Playlist successfully exported!');
+    } else {
+      throw new Error('Failed to export the playlist.');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert("There was an error generating your playlist. Please try again later.");
+  });
+});
 
-// Example usage of getRecommendations function
-//getRecommendations(0.5, 0.8, 0.4, 0.7, 0.1, 0.5);
 
-*/
+document.getElementById("newButton3").addEventListener("click", function() {
+  fetch('http://localhost:3000/generate-sad-playlist', {
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(trackDetails => {
+    if (Array.isArray(trackDetails)) {
+      hideAll();
+
+      const playlistContainer = document.getElementById('playlistContainer');
+      if (playlistContainer) {
+        playlistContainer.style.display = 'block';
+      }
+
+      const playlist = document.getElementById('playlist');
+      playlist.innerHTML = '';
+
+      trackDetails.forEach(track => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<img src="${track.albumCover}" class="album-cover"><span>${track.name} by ${track.artist}</span>`;
+        playlist.appendChild(listItem);
+      });
+
+      const playlistName = prompt('Enter a name for your playlist:');
+      return fetch('http://localhost:3000/export-playlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        },
+        body: JSON.stringify({
+          playlistName: playlistName,
+          trackURIs: trackDetails.map(track => track.uri)
+        })
+      });
+    } else {
+      console.error('Error: trackURIs is not an array:', trackDetails);
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Playlist successfully exported!');
+    } else {
+      throw new Error('Failed to export the playlist.');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert("There was an error generating your playlist. Please try again later.");
+  });
+});
+
+
+document.getElementById("newButton4").addEventListener("click", function() {
+  fetch('http://localhost:3000/generate-chill-playlist', {
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(trackDetails => {
+    if (Array.isArray(trackDetails)) {
+      hideAll();
+
+      const playlistContainer = document.getElementById('playlistContainer');
+      if (playlistContainer) {
+        playlistContainer.style.display = 'block';
+      }
+
+      const playlist = document.getElementById('playlist');
+      playlist.innerHTML = '';
+
+      trackDetails.forEach(track => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<img src="${track.albumCover}" class="album-cover"><span>${track.name} by ${track.artist}</span>`;
+        playlist.appendChild(listItem);
+      });
+
+      const playlistName = prompt('Enter a name for your playlist:');
+      return fetch('http://localhost:3000/export-playlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        },
+        body: JSON.stringify({
+          playlistName: playlistName,
+          trackURIs: trackDetails.map(track => track.uri)
+        })
+      });
+    } else {
+      console.error('Error: trackURIs is not an array.');
+      throw new Error('trackURIs is not an array.');
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(playlist => {
+    document.getElementById('playlistLink').href = playlist.external_urls.spotify;
+    document.getElementById('playlistCover').src = playlist.images[0].url;
+    document.getElementById('playlistCover').style.display = 'block';
+    document.getElementById('playlistLink').style.display = 'block';
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+});
